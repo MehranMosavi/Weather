3723b5bfc3e1ee8713180084be2e84942be05a1d
MehranMosavi <MehranOjaghi1992@gmail.com>
2020-09-19 21:10:41 +0330

    Update

--- app/src/main/java/com/example/weather/WeatherActivity.java
+++ app/src/main/java/com/example/weather/WeatherActivity.java
@@ -2,19 +2,32 @@ package com.example.weather;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.weather.Models.Weather;
+import com.example.weather.Remote.APIUtils;
+import com.example.weather.Remote.WeatherService;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 import static com.example.weather.Common.KEY_LAT;
 import static com.example.weather.Common.KEY_LON;
+import static com.example.weather.Common.OPENWEATHERMAP_TOKEN;
 
 public class WeatherActivity extends AppCompatActivity {
 
-    TextView txtLat,txtLon;
-    float lat,lon;
+
+    double lat,lon;
+    TextView txtCityName, txtTemp, txtPressure, txtDescription, txtWindSpeed, txtLat, txtLon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,17 +36,53 @@ public class WeatherActivity extends AppCompatActivity {
 
         init();
 
-        txtLat.setText("lat= "+ KEY_LAT);
-        txtLon.setText("lat= "+ KEY_LON);
+        WeatherService service = APIUtils.getWeatherService();
+        Call<Weather> call = null;
+
+        call = service.getCurrentWeather(lat,lon,OPENWEATHERMAP_TOKEN,"metric");
+
+        call.enqueue(new Callback<Weather>() {
+            @Override
+            public void onResponse(Call<Weather> call, Response<Weather> response) {
+                if(response.isSuccessful())
+                {
+                    Weather weather = response.body();
+                    txtCityName.setText(weather.getName());
+                    txtTemp.setText(weather.getMain().getTemp() + " ℃");
+                    txtPressure.setText(weather.getMain().getPressure() + "");
+                    txtDescription.setText("NULL");
+                    txtWindSpeed.setText(weather.getWind().getSpeed()+"");
+                }
+                else
+                {
+                    Toast.makeText(WeatherActivity.this, "ERROR", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Weather> call, Throwable t) {
+                Toast.makeText(WeatherActivity.this, "NOT CONNECTED", Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 
     public void init()
     {
-        txtLat = findViewById(R.id.txtlat);
-        txtLon = findViewById(R.id.txtlon);
+        txtCityName = findViewById(R.id.weatherActivity_txt_CityName);
+        txtTemp = findViewById(R.id.weatherActivity_txt_Temp);
+        txtPressure = findViewById(R.id.weatherActivity_txt_Pressure);
+        txtDescription = findViewById(R.id.weatherActivity_txt_Description);
+        txtWindSpeed = findViewById(R.id.weatherActivity_txt_WindSpeed);
+        txtLat = findViewById(R.id.weatherActivity_txt_Latitude);
+        txtLon = findViewById(R.id.weatherActivity_txt_Longitude);
 
-        lat = getIntent().getIntExtra(KEY_LAT,0);
-        lon = getIntent().getIntExtra(KEY_LON,0);
+        Intent intent = getIntent();
+        lat = getIntent().getDoubleExtra(KEY_LAT,0);
+        lon = getIntent().getDoubleExtra(KEY_LON,0);
+
+        txtLat.setText(""+lat);
+        txtLon.setText(""+lon);
     }
+
 }
